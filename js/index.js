b8508674365a8be2994c2f9c3309e0939a6059a7
Aliaksei Karzhou <aliaksei_karzhou1@epam.com>
2023-10-27 23:23:26 +0300

    fix: fixed mobile nav dropdown

--- js/index.js
+++ js/index.js
@@ -1,12 +1,22 @@
-import './catalog.js'
-
+import './catalog.js';
 
 const hamburgerBtn = document.querySelector('.header__button_hamburger');
 const headerNav = document.querySelector('.header .nav');
 
-
 hamburgerBtn.addEventListener('click', () => {
-  document.body.classList.toggle('active')
+  document.body.classList.toggle('active');
   headerNav.classList.toggle('active');
   hamburgerBtn.classList.toggle('active');
+});
+
+const headerNavDropdown = document.querySelectorAll('.nav__menu-item_dropdown');
+
+headerNavDropdown.forEach((elem) => {
+  elem.addEventListener('click', (event) => {
+    const isTargetDropdownButton = event.target.classList.contains('nav__menu-button-dropdown');
+    if (!isTargetDropdownButton) return;
+    const dropdownButtonElem = event.target;
+    const submenuElem = event.currentTarget.querySelector('.nav__submenu');
+    submenuElem.classList.toggle('active');
+  });
 });
