4351694cd2ba317b9a4ae8ef28cccc804e79e1ad
Aliaksei Karzhou <aliaksei_karzhou1@epam.com>
2023-10-29 23:19:50 +0300

    feat: implemented product page

--- js/index.js
+++ js/index.js
@@ -22,19 +22,56 @@ headerNavDropdown.forEach((elem) => {
   });
 });
 
-const firstScreenForm = document.querySelector('.first-screen__form');
+const firstScreenForms = document.querySelectorAll('.first-screen__form');
 
-firstScreenForm.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const form = new FormData(event.currentTarget);
+firstScreenForms.forEach((firstScreenForm) => {
+  firstScreenForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const form = new FormData(event.currentTarget);
 
-  const typeOfObject = form.get('typeOfObject');
-  const area = form.get('area');
-  const bedrooms = form.get('bedrooms');
-  const price = form.get('price');
-  const more = form.get('more');
+    const typeOfObject = form.get('typeOfObject');
+    const area = form.get('area');
+    const bedrooms = form.get('bedrooms');
+    const price = form.get('price');
+    const more = form.get('more');
 
-  console.log(
-    `typeOfObject: ${typeOfObject} area: ${area} bedrooms: ${bedrooms} price: ${price} more: ${more}`
-  );
+    console.log(
+      `typeOfObject: ${typeOfObject} area: ${area} bedrooms: ${bedrooms} price: ${price} more: ${more}`
+    );
+  });
+});
+
+const productSliders = document.querySelectorAll('.product__slider');
+
+productSliders.forEach((productSlider) => {
+  const mainSlider = productSlider.querySelector('.product__slider-main');
+  const thumbsSlider = productSlider.querySelector('.product__slider-thumbs');
+
+  const prevButton = productSlider.querySelector('.swiper-button-prev');
+  const nextButton = productSlider.querySelector('.swiper-button-next');
+
+  const swiperThumbs = new Swiper(thumbsSlider, {
+    spaceBetween: 8,
+    slidesPerView: 5.2,
+    freeMode: true,
+    watchSlidesProgress: true,
+    autoScrollOffset: 1,
+
+    breakpoints: {
+      576: {
+        slidesPerView: 6,
+      },
+    },
+  });
+
+  const swiperMain = new Swiper(mainSlider, {
+    spaceBetween: 10,
+    navigation: {
+      nextEl: nextButton,
+      prevEl: prevButton,
+    },
+    thumbs: {
+      swiper: swiperThumbs,
+    },
+  });
 });
